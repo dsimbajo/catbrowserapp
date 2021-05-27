dbb2b21ff47ddbc117f5ecc772bb0e23a83e1f75
Darwin Simbajon <darwin.simbajon@yahoo.com>
2021-05-28 00:16:13 +0800

    Added API service and model for catbreed - Added CatBreed.ts (model) - Added CatAPI.ts (API service to fetch data from provided API)

---
+++ src/models/CatBreed.ts
@@ -0,0 +1,7 @@
+export interface ICatBreed{
+    id: string,
+    name: string,
+    origin: string,
+    temperament: string,
+    image: any
+}
---
+++ src/services/CatAPI.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios';
+import { ICatBreed } from '../models/CatBreed'
+
+const instance = axios.create({
+    baseURL: "https://api.thecatapi.com/"
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const request = {
+    get: (url:string) => instance.get(url).then(responseBody)
+}
+
+export const CatAPI = {
+    getCatBreeds: (): Promise<ICatBreed[]> => request.get("v1/breeds"),
+    searchCatBreed: (breedId: string): Promise<ICatBreed[]> => request.get(`v1/breeds/search?q=${breedId}`),
+    searchBreedImages: (breedId: string): Promise<ICatBreed[]> => request.get(`v1/images/search?breed_id${breedId}&limit=100`)
+}
+
